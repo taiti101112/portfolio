122a370d9e582ef8cbcfdf6fc258a13b74213659
taiti101112 <bbbstaiti@gmail.com>
2024-05-10 17:10:38 +0900

    管理画面作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :shops
+  # 一般ユーザーがアクセスできるショップの一覧と詳細
+  resources :shops, only: [:index, :show]
+
+  # 管理画面用のルーティング
+  namespace :admin do
+    resources :shops
+  end
 end
