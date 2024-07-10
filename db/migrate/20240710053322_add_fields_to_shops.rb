f5d06968e4605a56fcbc7e3e604d339295c40307
taiti101112 <bbbstaiti@gmail.com>
2024-07-10 14:52:15 +0900

    ショップテーブルにsnsのカラムを追加

---
+++ db/migrate/20240710053322_add_fields_to_shops.rb
@@ -0,0 +1,7 @@
+class AddFieldsToShops < ActiveRecord::Migration[7.0]
+  def change
+    add_column :shops, :twitter, :string
+    add_column :shops, :instagram, :string
+    add_column :shops, :official_hp, :string
+  end
+end
