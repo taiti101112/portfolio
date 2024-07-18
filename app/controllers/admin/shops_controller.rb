22a52e47d22d73abfc3bf2dbb627754104a283ff
taiti101112 <bbbstaiti@gmail.com>
2024-07-18 15:58:51 +0900

    shopに追加したカラムをビューに反映

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -3,10 +3,22 @@ module Admin
     before_action :authenticate_admin! # 管理者認証を有効にする
 
     def index
-      @shops = Shop.all
+      @q = Shop.ransack(params[:q])
+      @is_admin = current_user.admin?
+      if params[:q].present?
+        @shops = @q.result(distinct: true)
+      else
+        @shops = Shop.all
+      end
+    
+      # タグで絞り込む
+      if params[:tag_name]
+        @shops = @shops.tagged_with(params[:tag_name])
+      end
     end
 
     def show
+      @is_admin = current_user.admin?
       @shop = Shop.find(params[:id])
       @tags = @shop.tag_counts_on(:tags)
     end
@@ -21,9 +33,11 @@ module Admin
 
     def create
       @shop = Shop.new(shop_params)
+      @shop.user_id = current_user.id
       if @shop.save
         redirect_to [:admin, @shop], notice: 'ショップの作成が完了しました'
       else
+        Rails.logger.error(@shop.errors.full_messages)
         render :new
       end
     end
@@ -46,7 +60,9 @@ module Admin
     private
 
     def shop_params
-      params.require(:shop).permit(:name, :address, :phone_number, :duel_space_available, :opening_hours, :tag_list,)
+      params.require(:shop).permit(:name, :address, :phone_number, :duel_space_available, :opening_hours, :tag_list, :latitude, :longitude, :user_id)
     end
+    
+
   end
 end
---
+++ db/migrate/20240718021250_change_phone_number_to_string_in_shops.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumberToStringInShops < ActiveRecord::Migration[7.0]
+  def change
+    change_column :shops, :phone_number, :string
+  end
+end
