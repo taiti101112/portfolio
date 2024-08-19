bac110394e04668a7ca656956172fe29b07011e2
taiti101112 <bbbstaiti@gmail.com>
2024-08-19 15:06:45 +0900

    ページネーション機能を実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,27 +5,30 @@ class ShopsController < ApplicationController
 
   def index
     @q = Shop.ransack(params[:q])
-    
+  
+    # 初期クエリの作成
     if params[:q].present?
       @shops = @q.result(distinct: true).includes(:business_hours)
-      gon.shops = @q.result(distinct: true).includes(:business_hours)
     else
       @shops = Shop.includes(:business_hours).all
-      gon.shops = Shop.includes(:business_hours).all
     end
   
-    # タグで絞り込む
+    # タグで絞り込み
     if params[:tag_name]
       @shops = @shops.tagged_with(params[:tag_name])
-      gon.shops = @shops.tagged_with(params[:tag_name])
     end
   
     # 営業中の店舗のみをフィルタリング
     if params[:open_now] == '1'
-      @shops = @shops.select { |shop| shop.open_now? }
-      gon.shops = @shops.select { |shop| shop.open_now? }
+      @shops = @shops.open_now
     end
+  
+    # 配列オブジェクトをページネーション対応
+    @shops = @shops.page(params[:page]).per(5)
+    # JavaScript用のデータを設定
+    gon.shops = @shops
   end
+  
 
   def show
     @shop = Shop.find(params[:id])
