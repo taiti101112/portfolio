1c32a740f8a1305bb3144b477243ef0fc8162fec
taiti101112 <bbbstaiti@gmail.com>
2024-08-28 16:44:26 +0900

    cssを反映させるよう修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,5 @@
 // app/assets/config/manifest.js
 
 //= link_tree ../images
-//= link_tree ../builds
 //= link_directory ../stylesheets .css
 //= link application.js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
