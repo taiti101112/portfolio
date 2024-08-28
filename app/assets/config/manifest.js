9be2ec3407c2b55e6cacc1d2914dbfc59b60d7f7
taiti101112 <bbbstaiti@gmail.com>
2024-08-28 15:30:21 +0900

    esbuildからimportmapに変更

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
+// app/assets/config/manifest.js
+
 //= link_tree ../images
 //= link_tree ../builds
+//= link_directory ../stylesheets .css
+//= link application.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// app/javascript/application.js
+
+// Importmapを使ってピン留めされたモジュールをインポート
+import "@hotwired/turbo-rails";
+import { Application } from "@hotwired/stimulus";
+
+// Stimulusの設定
+const application = Application.start();
