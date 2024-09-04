d0fdb664c8879afdd17748f682cb56133c2c92b1
taiti101112 <bbbstaiti@gmail.com>
2024-09-04 17:39:29 +0900

    userコントローラの不要なアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,34 +8,13 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def new 
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render "new"
-    end
-  end
-
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
   end
 
-  def set_user
-    return if params[:id] == "sign_out" # ログアウトリクエストをスキップ
-  
+  def set_user  
     @user = User.find(params[:id])
   end
 
