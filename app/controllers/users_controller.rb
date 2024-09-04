f456a3ced3de21090ec1474503cd907195f4eede
taiti101112 <bbbstaiti@gmail.com>
2024-09-04 17:59:02 +0900

    エラーが出ないコミット(add_admin)まで巻き戻し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,6 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def new 
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render "new"
-    end
-  end
-
   private
 
   def user_params
