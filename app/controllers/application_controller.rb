3fff18d121fd4d3c830216209f2d7653bce92205
taiti101112 <bbbstaiti@gmail.com>
2024-09-06 14:38:56 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_ransack_search_object
   before_action :configure_permitted_parameters, if: :devise_controller? 
 
-
-master
   protected
 
-  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
