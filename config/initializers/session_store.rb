bf05bbfecf65bd72348b42e0551f2de58eafe452
taiti101112 <bbbstaiti@gmail.com>
2024-09-06 14:21:28 +0900

    session_store.rbの記載を削除

--- config/initializers/session_store.rb
+++
@@ -1,2 +0,0 @@
-# config/initializers/session_store.rb
-Rails.application.config.session_store :cookie_store, key: '_tcg_place_session', secure: Rails.env.production?, same_site: :lax, domain: :all, tld_length: 2
