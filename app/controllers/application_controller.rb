40dc88a69dbb898d5ebc486fa3703e0c336df08d
taiti101112 <bbbstaiti@gmail.com>
2024-09-06 10:36:22 +0900

    protect_from_forgery with: :exceptionを記載してセッションを確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_ransack_search_object
   before_action :configure_permitted_parameters, if: :devise_controller? 
 
+  protect_from_forgery with: :exception
+
   protected
 
   def configure_permitted_parameters
