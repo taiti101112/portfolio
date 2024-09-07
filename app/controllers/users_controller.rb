7ea66b089265c1985cfeb5490c6df0acdad772c1
taiti101112 <bbbstaiti@gmail.com>
2024-09-07 17:49:56 +0900

    user/showを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
 
   def show
-    @user = User.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
+    @user = User.find_by(id: current_user.id)
   end
 
   private
