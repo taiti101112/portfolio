5b9dcffd2d5d97c9151d078cc412845118f16709
taiti101112 <bbbstaiti@gmail.com>
2024-09-07 12:17:32 +0900

    session_storeのコメントアウトを削除

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # config/initializers/session_store.rb
 # 以下の記述があるとログインはできるがログアウトできない
 # 以下の記述がないとログアウトはできるがログインができない
-Rails.application.config.session_store :cookie_store, key: '_tcg_place_session', secure: Rails.env.production?, same_site: :none, domain: :all, tld_length: 2
-# Rails.application.config.session_store :cookie_store, key: '_tcg_place_session', secure: Rails.env.production?, same_site: nil
+# Rails.application.config.session_store :cookie_store, key: '_tcg_place_session', secure: Rails.env.production?, same_site: :none, domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_tcg_place_session', secure: Rails.env.production?, same_site: nil
 
