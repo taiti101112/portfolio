b512e9d10fdeaccea3cf9c0a1f104b2fe16731cc
taiti101112 <bbbstaiti@gmail.com>
2024-09-18 16:08:49 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,4 @@ pin "@rails/ujs", to: "rails-ujs.js"
 pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@7.3.0/dist/turbo.es2017-umd.js"
 pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.1/dist/stimulus.umd.js"
 
-# 全てのコントローラーを自動的にピンする
-pin "controllers/application", to: "controllers/application.js"
-pin "controllers/hello_controller", to: "controllers/hello_controller.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
