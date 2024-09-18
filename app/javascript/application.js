b279d9a25984bf51ad0be2685066b94dc8dbed1f
taiti101112 <bbbstaiti@gmail.com>
2024-09-18 15:12:45 +0900

    コンソールエラーの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,10 @@
 
 import { Turbo } from "@hotwired/turbo-rails";
 import Rails from "@rails/ujs";
-import "controllers";
+
+// 変更箇所: 相対パスで各コントローラーを明示的にインポート
+import "./controllers/application";
+import "./controllers/hello_controller";
 
 Turbo.session.drive = true; // Turboの動作を有効にします
 Rails.start();
