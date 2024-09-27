f5af931ff6749fd88dc57a707c66c1d83a819524
taiti101112 <bbbstaiti@gmail.com>
2024-09-27 11:12:43 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,10 @@
 import { Turbo } from "@hotwired/turbo-rails";
 
-// rails-ujsを相対パスで読み込む
-import Rails from "../node_modules/@rails/ujs";
+// rails-ujs はImportmapやESMモジュールとして読み込まず、Sprockets で管理
 Rails.start();
 
 // Stimulus コントローラーのインポートと登録
-import { application } from "controllers/application";  
+import { application } from "controllers/application";
 import HelloController from "controllers/hello_controller";
 application.register("hello", HelloController);
 
