be07ea96516d214d81bf91ee4b6b8eabf35fb193
taiti101112 <bbbstaiti@gmail.com>
2024-09-27 11:25:19 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,8 @@
-# config/importmap.rb
 pin "application"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"
 pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@7.3.0/dist/turbo.es2017-umd.js"
 
-if Rails.env.production?
-  pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.1/dist/stimulus.js"
-else
-  pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.1/dist/stimulus.umd.js"
-end
+pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.1/dist/stimulus.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
