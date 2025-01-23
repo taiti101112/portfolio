d17712ee40d8364df09bb3e34cfae08fa1fb3eee
taiti101112 <bbbstaiti@gmail.com>
2025-01-23 22:02:24 +0900

    パスワード変更関連のビューを修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,6 +4,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_email(name, email, message)
     @name = name
+    @email = email
     @message = message
     mail(to: 'sakauchi.runteq@gmail.com', subject: 'お問い合わせ')
   end
