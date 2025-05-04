e5c8d43ae8306902d7278de8b39df7eb5e1d1c5d
taiti101112 <bbbstaiti@gmail.com>
2025-05-04 14:25:12 +0900

    BusinessHourモデルのテスト項目を日本語に翻訳

--- spec/models/business_hour_spec.rb
+++ spec/models/business_hour_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BusinessHour, type: :model do
   let(:shop) { create(:shop) }
 
-  it 'is valid with valid attributes' do
+  it 'BusinessHourモデルが正しく作成されること' do
     business_hour = BusinessHour.new(
       shop: shop,
       day_of_week: 1,
@@ -13,19 +13,19 @@ RSpec.describe BusinessHour, type: :model do
     expect(business_hour).to be_valid
   end
 
-  it 'is invalid without a day_of_week' do
+  it 'BusinessHourモデルにday_of_weekが存在しない場合にバリデーションが働くこと' do
     business_hour = BusinessHour.new(shop: shop, opening_time: '09:00', closing_time: '18:00')
     business_hour.day_of_week = nil
     expect(business_hour).to_not be_valid
   end
 
-  it 'is invalid without an opening_time' do
+  it 'BusinessHourモデルにopening_timeが存在しない場合にバリデーションが働くこと' do
     business_hour = BusinessHour.new(shop: shop, day_of_week: 1, closing_time: '18:00')
     business_hour.opening_time = nil
     expect(business_hour).to_not be_valid
   end
 
-  it 'is invalid without a closing_time' do
+  it 'BusinessHourモデルにclosing_timeが存在しない場合にバリデーションが働くこと' do
     business_hour = BusinessHour.new(shop: shop, day_of_week: 1, opening_time: '09:00')
     business_hour.closing_time = nil
     expect(business_hour).to_not be_valid
