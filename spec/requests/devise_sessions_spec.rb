2c3272300447dfafd37121e520dd4b4fbf1a742d
taiti101112 <bbbstaiti@gmail.com>
2025-05-04 15:30:28 +0900

    rspecのテスト項目を日本語に翻訳

--- spec/requests/devise_sessions_spec.rb
+++ spec/requests/devise_sessions_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'Devise Authentication', type: :request do
+RSpec.describe 'Devise認証', type: :request do
   let!(:user) do
     create(:user, name: 'test_user', email: 'test@example.com', password: 'password', password_confirmation: 'password')
   end
 
-  describe 'POST /users/sign_in' do
-    it 'logs in the user' do
+  describe 'ログイン' do
+    it 'ログインできること' do
       post user_session_path, params: { user: { email: user.email, name: user.name, password: 'password' } }
       expect(response).to redirect_to(user_path(user))
     end
   end
 
-  describe 'DELETE /users/sign_out' do
+  describe 'ログアウト' do
     before do
       sign_in user
     end
 
-    it 'logs out the user' do
+    it 'ログアウトできること' do
       delete destroy_user_session_path
       expect(response).to redirect_to(root_path)
     end
