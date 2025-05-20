f14907b1be1d2956cf826e4fd4c276986433512e
taiti101112 <bbbstaiti@gmail.com>
2025-05-20 18:47:27 +0900

    Javascriptの設定を修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link application.js
-//= link rails-ujs.js
 //= link controllers/application.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin 'bootstrap', to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/boots
 pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js'
 pin '@hotwired/turbo-rails', to: 'https://cdn.jsdelivr.net/npm/@hotwired/turbo@7.3.0/dist/turbo.es2017-umd.js'
 pin '@hotwired/stimulus', to: 'https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.1/dist/stimulus.js'
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3-4/app/assets/javascripts/rails-ujs.esm.js"
 pin_all_from 'app/javascript/controllers', under: 'controllers'
